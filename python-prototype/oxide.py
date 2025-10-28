2f7912396cfbc4c8da694b8508fe1eec9f8b8a89
Sam P. <Samjpoyner@icloud.com>
2025-10-28 18:48:57 -0400

    Adjust test string

--- python-prototype/oxide.py
+++ python-prototype/oxide.py
@@ -2,7 +2,7 @@ from lexer import Lexer, TOKENTYPES
 from resolver import Resolver, DEFS
 
 # Yes, I know this test string is bad. No, I don't regret it. No, I'm not going to change it :P
-c='"Hello, world"("here\'s a group with no leading space") [have an array] {and an expression}a=b (+={this[is+a (test)]})"and this is another one"'
+c='"Hello, world"("here\'s a group with no leading space") [have an array] {and an expression}a=b- /=(+={this[is+a (test)]})"and this is another one"'
 
 l=Lexer(c)
 print("Lexing")
